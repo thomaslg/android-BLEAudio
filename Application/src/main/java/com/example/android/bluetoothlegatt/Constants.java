ee3abecdce071db069b1a2dc58e874474bd23fe1
Thomas LeGuevel <thomasleguevel@gmail.com>
2016-02-02 03:13:41 +0100

    Update the application to use the BLE COC

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/Constants.java
@@ -0,0 +1,9 @@
+package com.example.android.bluetoothlegatt;
+
+/**
+ * Created by thomasleguevel on 27/01/16.
+ */
+public class Constants {
+    public static final String FOLDER = "/Development/NXP_BLE";
+    public static final String FILE = "/input.pcm";
+}
